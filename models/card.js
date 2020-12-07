bf5a4c3bc3c5f1ce9b45fad5dded883c13ae7862
Derek Schinke <derekschinke@me.com>
2020-12-07 11:59:58 -0800

    fix: removed schema versioning

--- models/card.js
+++ models/card.js
@@ -1,34 +1,37 @@
 const mongoose = require('mongoose');
 
-const schema = mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 2,
-    maxLength: 30,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: true,
-    validate: {
-      validator(v) {
-        const regex = /^https?:\/\/(www\.)?[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;%=]+#?$/;
-        return regex.test(v);
+const schema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minLength: 2,
+      maxLength: 30,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+      validate: {
+        validator(v) {
+          const regex = /^https?:\/\/(www\.)?[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;%=]+#?$/;
+          return regex.test(v);
+        },
       },
     },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      default: [],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  likes: {
-    type: [mongoose.Schema.Types.ObjectId],
-    default: [],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model('card', schema);
