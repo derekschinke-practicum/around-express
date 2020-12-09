5cd375e07577cb73de1bbb321c60bd0fd17c36b8
Derek Schinke <derekschinke@me.com>
2020-12-08 23:25:58 -0800

    feature: error handling

--- controllers/card.js
+++ controllers/card.js
@@ -1,23 +1,20 @@
 const Card = require('../models/card');
 
+const handleError = require('../helpers/handleError');
+
 module.exports.getCards = (req, res) =>
   Card.find({})
     .then((cards) => res.status(200).send(cards))
-    .catch((err) => res.status(500).send({ message: err }));
+    .catch((err) => handleError(err, res, 'card'));
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(200).send(card))
-    .catch((err) => res.status(500).send({ message: err }));
+    .then((cards) => res.status(200).send(cards))
+    .catch((err) => handleError(err, res, 'card'));
 };
 
 module.exports.deleteCard = (req, res) =>
   Card.findByIdAndDelete(req.params._id)
-    .then((card) => {
-      if (card) {
-        return res.status(200).send();
-      }
-      return res.status(404).send({ message: 'Card ID not found' });
-    })
-    .catch((err) => res.status(500).send({ message: err }));
+    .then((cards) => res.status(200).send(cards))
+    .catch((err) => handleError(err, res, 'card'));
--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,20 @@
 const User = require('../models/user');
 
+const handleError = require('../helpers/handleError');
+
 module.exports.getUsers = (req, res) =>
   User.find({})
     .then((users) => res.status(200).send(users))
-    .catch((err) => res.status(400).send({ message: err }));
+    .catch((err) => handleError(err, res, 'user'));
 
 module.exports.getUserById = (req, res) =>
   User.findById(req.params.id)
-    .then((user) => {
-      if (user) {
-        return res.status(200).send(user);
-      }
-      return res.status(404).send({ message: 'User ID not found' });
-    })
-    .catch((err) => res.status(500).send({ message: err }));
+    .then((user) => res.status(200).send(user))
+    .catch((err) => handleError(err, res, 'user'));
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.status(200).send(user))
-    .catch((err) => res.status(500).send({ message: err }));
+    .catch((err) => handleError(err, res, 'user'));
 };
