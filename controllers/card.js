69f0fe2f9667113cc0144e6d6c67411ecf918fd3
Derek Schinke <derekschinke@me.com>
2020-12-08 23:55:57 -0800

    fix: card controller plurals

--- controllers/card.js
+++ controllers/card.js
@@ -4,17 +4,17 @@ const handleError = require('../helpers/handleError');
 
 module.exports.getCards = (req, res) =>
   Card.find({})
-    .then((card) => res.status(200).send(card))
+    .then((cards) => res.status(200).send(cards))
     .catch((err) => handleError(err, res, 'card'));
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((cards) => res.status(200).send(cards))
+    .then((card) => res.status(200).send(card))
     .catch((err) => handleError(err, res, 'card'));
 };
 
 module.exports.deleteCard = (req, res) =>
   Card.findByIdAndDelete(req.params._id)
-    .then((cards) => res.status(200).send(cards))
+    .then((card) => res.status(200).send(card))
     .catch((err) => handleError(err, res, 'card'));
